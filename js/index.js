e9bc02f27f5f8e16b85e076bf7ed6e9f917baaca
carpinx <colettamatias@gmail.com>
2022-02-21 18:43:52 -0300

    Choose another language button added

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ let buttonEs = document.querySelector("#es").addEventListener("click", nameEs);
 let buttonFr = document.querySelector("#fr").addEventListener("click", nameFr);
 let buttonGa = document.querySelector("#ga").addEventListener("click", nameGa);
 
+let buttonBackToLang = document.querySelector(".buttonBackToLang");
+buttonBackToLang.addEventListener("click", backToLang);
+
 let writeName = document.querySelector(".writeName");
 writeName.addEventListener("submit", submitName);
 
@@ -11,6 +14,19 @@ function hideButtonsLang() {
     buttons.forEach((button) => {
         button.classList.add("hidden");
     });
+    buttonBackToLang.classList.remove("hidden");
+}
+
+function showButtonsLang() {
+    buttons.forEach((button) => {
+        button.classList.remove("hidden");
+    });
+}
+
+function backToLang() {
+    showButtonsLang();
+    writeName.classList.add("hidden");
+    buttonBackToLang.classList.add("hidden");
 }
 
 function submitName(event) {
@@ -24,6 +40,8 @@ function submitName(event) {
 function nameEn() {
     hideButtonsLang();
     writeName.classList.remove("hidden");
+    buttonBackToLang.classList.remove("hidden");
+    buttonBackToLang.textContent = "Choose another language";
     writeName.children[0].textContent = "Write your name";
     writeName.children[2].textContent = "Start";
     sessionStorage.setItem("lang", "en");
@@ -32,6 +50,8 @@ function nameEn() {
 function nameEs() {
     hideButtonsLang();
     writeName.classList.remove("hidden");
+    buttonBackToLang.classList.remove("hidden");
+    buttonBackToLang.textContent = "Escoge otro idioma";
     writeName.children[0].textContent = "Escribe tu nombre";
     writeName.children[2].textContent = "Iniciar";
     sessionStorage.setItem("lang", "es");
@@ -40,6 +60,8 @@ function nameEs() {
 function nameFr() {
     hideButtonsLang();
     writeName.classList.remove("hidden");
+    buttonBackToLang.classList.remove("hidden");
+    buttonBackToLang.textContent = "Choisir une autre langue";
     writeName.children[0].textContent = "Écris ton prénom";
     writeName.children[2].textContent = "Commencer";
     sessionStorage.setItem("lang", "fr");
@@ -48,6 +70,8 @@ function nameFr() {
 function nameGa() {
     hideButtonsLang();
     writeName.classList.remove("hidden");
+    buttonBackToLang.classList.remove("hidden");
+    buttonBackToLang.textContent = "Escolle outro idioma";
     writeName.children[0].textContent = "Escribe o teu nome";
     writeName.children[2].textContent = "Comezar";
     sessionStorage.setItem("lang", "ga");
